8e19184f1bd48336cd8dd5cb56587eefc3328d35
K4RI <kariassoumani@hotmail.fr>
2024-04-06 16:19:32 +0200

    contact avec sentry

--- scripts/js/contact.js
+++ scripts/js/contact.js
@@ -6,17 +6,19 @@ const formulaire = document.getElementById("formulaire")
 
 formulaire.addEventListener('submit', (event) => {
     event.preventDefault();
+    alert('Message envoyé !');
     setTimeout(function(){
         window.location.href = 'index.html'
-    }, 2000);
-    Email.send({
-        SecureToken : "25f715e6-9738-44fc-99c8-1c91cf229766", // oui je sais mettre des credentials en clair c'est pas bien
-        To : "kariassoumani@hotmail.fr", // mais l'accès donné est à une adresse e-mail jetable donc je m'en fous lol
-        From : "juj222@proton.me", // <-- à cette adresse celle-là
-        Subject : `[JEJ888] ${tTitre.value}`,
-        Body : `${tMessage.value.replaceAll('\n', '<br>')} <br><br>
-        -- ${tNom.value}, ${tContact.value}` // en attendant d'avoir du back-end sur le serveur mdr allez la bise
-    }).then(
-      message => alert('Message envoyé !')
-    );
+    }, 1000);
+    
+    // https://docs.sentry.io/platforms/javascript/user-feedback/
+    // https://docs.sentry.io/platforms/javascript/user-feedback/configuration/
+    const eventId = Sentry.captureMessage("User Feedback");
+    const userFeedback = {
+        event_id: eventId,
+        name: tNom.value,
+        email: tContact.value,
+        comments: tMessage.value, // tTitre.value
+    };
+    Sentry.captureUserFeedback(userFeedback);
 })
