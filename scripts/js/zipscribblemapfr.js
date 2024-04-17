3e064aa80a3c7cea7126be16723b404bc10ba24b
K4RI <kariassoumani@hotmail.fr>
2024-04-17 12:40:40 +0200

    zipscribblemap speed

--- scripts/js/zipscribblemapfr.js
+++ scripts/js/zipscribblemapfr.js
@@ -21,6 +21,16 @@ let boutonReinit = document.getElementById("reinit");
 let selectRes = document.getElementById("resolution");
 let boutonTelecharger = document.getElementById("telecharger");
 let canvas = document.querySelector(".app-canvas");
+let loading = document.getElementById("loading");
+
+
+checkDeps.checked = false // on initialise à rien cocher
+selectRes.value = '1080';
+
+boutonTracer.disabled = true;
+boutonReinit.disabled = true;
+selectRes.disabled = true;
+boutonTelecharger.disabled = true;
 
 
 /** association des boutons aux paramètres */
@@ -51,41 +61,72 @@ boutonTelecharger.addEventListener("click", (event) => {
     Plotly.downloadImage(canvas, {format: 'png', width: resolution, height: resolution, filename: `zipscribblemap_${resolution}x${resolution}`});
 })
 
-// let relinLat = x => 8.953755342571714e-6*x -12.700790555447483
-// let relinLong = x => 1.2979400576048456e-5*x -5.832552696046815
-// let relinLat = x => 8.9538e-6*x -12.7008
-// let relinLong = x => 1.298e-5*x -5.8326
 
+/** initialisation du chemin et des données */
 let relinLat = x => 9.067125875160348e-6*x -13.362329239550334
 let relinLong = x => 1.2408828030287988e-5*x -5.7279126035491315
 
-// let relinLat = x => 9.141564715007134e-6*x-13.920168834331918
-// let relinLong = x => 1.2816372422366154e-5*x -6.376071970236577
+async function initPathRecords(){
+    let depLong = x => (0.0294*x -5).toFixed(3)
+    let depLat = x => (0.0215*x + 52.23).toFixed(3)
+
+    let pathStr = 'M '
+    await $.get('../../jeux/france_vect.txt', function( data ) { // on lit le texte du csv
+        const header = 'x0 y0 x1 y1\n'
+        let records = $.csv.toObjects(header + data, {separator: " "});    
+        for (let i = 0; i < records.length - 1; i++) {
+            pathStr += `${depLong(records[i]['x0'])},${depLat(-records[i]['y0'])} L`
+        }
+    })
+    
+    // lire le csv
+    let records;
+    await $.get('../../jeux/villes_utf8.csv', function( text ) {
+        const header = 'index;code_commune;code_postal;nom;habitants;densite;latitude;longitude;x;y\n'
+        records = $.csv.toObjects(header + text, {separator: ";"});
+    });
+    
+    // séparer les entrées par département
+    let separatedRecords = {};
+    for (let i = 0; i < records.length; i++) {
+        if(records[i]['nom'] == 'Asprières'){ // les dernières entrées qui déconnent
+            break;
+        }
+        let dep = ~~(records[i]['code_postal']/1000)
+        if (!(dep in separatedRecords)){
+            separatedRecords[dep] = [records[i]]
+        } else {
+            separatedRecords[dep].push(records[i])
+        }
+    }
+    // trier par code postal
+    for (let key in separatedRecords){
+        separatedRecords[key] = separatedRecords[key].sort((a, b) => a['code_postal'] - b['code_postal']);
+    }
+    
+    return [pathStr, separatedRecords]
+}
+
+selectRes.dispatchEvent(new Event("change"));
+checkDeps.dispatchEvent(new Event("change"));
+sliderWidth.dispatchEvent(new Event("change"));
+sliderWidthD.dispatchEvent(new Event("change"));
+
+const [path, records] = await initPathRecords();
+loading.style.display = 'none';
+boutonTracer.disabled = false;
+boutonReinit.disabled = false;
 
 boutonTracer.addEventListener("click", async (event) => {
-    let data = []
-    let shapes = []
+    let shapes = afficheDeps ? [
+        {
+            type: 'path',
+            path: path,
+            line: {color: 'black', width: wd}
+        }
+    ] : [];
 
-    let depLong = x => (0.0294*x -5).toFixed(3)
-    let depLat = x => (0.0215*x + 52.23).toFixed(3)    
-
-    if (afficheDeps) {
-        await $.get('../../jeux/france_vect.txt', function( data ) { // on lit le texte du csv
-            const header = 'x0 y0 x1 y1\n'
-            const records = $.csv.toObjects(header + data, {separator: " "});
-            let pathStr = 'M '
-            for (let i = 0; i < records.length - 1; i++) {
-                pathStr += `${depLong(records[i]['x0'])},${depLat(-records[i]['y0'])} L`
-            }
-            shapes = [{
-                type: 'path',
-                path: pathStr,
-                line: {color: 'black', width: wd}
-            }]
-        })
-    }
-
-    var layout = {
+    let layout = {
         shapes: shapes,
         xaxis: {
             showgrid: true, // retirer la grille verticale
@@ -108,37 +149,14 @@ boutonTracer.addEventListener("click", async (event) => {
         }
     }
 
-    // lire le csv
-    let records;
-    await $.get('../../jeux/villes_utf8.csv', function( text ) {
-        const header = 'index;code_commune;code_postal;nom;habitants;densite;latitude;longitude;x;y\n'
-        records = $.csv.toObjects(header + text, {separator: ";"});
-    });
-
-    
-    // séparer les entrées par département
-    let separatedRecords = {};
-    for (let i = 0; i < records.length; i++) {
-        if(records[i]['nom'] == 'Asprières'){ // les dernières entrées qui déconnent
-            break;
-        }
-        let dep = ~~(records[i]['code_postal']/1000)
-        if (!(dep in separatedRecords)){
-            separatedRecords[dep] = [records[i]]
-        } else {
-            separatedRecords[dep].push(records[i])
-        }
-    }
-
-    // dans chaque liste départementale, trier par code postal
-    for (let key in separatedRecords){
-        separatedRecords[key] = separatedRecords[key].sort((a, b) => a['code_postal'] - b['code_postal']);
-        // et l'entrer dans le data
+    let data = []
+    // dans chaque liste départementale, l'entrer dans le data
+    for (let key in records){
         let xs = [], ys = [], names = [];
-        for (let c in separatedRecords[key]){
-            xs.push(relinLong(separatedRecords[key][c]['x']));
-            ys.push(relinLat(separatedRecords[key][c]['y']));
-            names.push(`${separatedRecords[key][c]['nom']} - ${separatedRecords[key][c]['code_postal']}`);
+        for (let c in records[key]){
+            xs.push(relinLong(records[key][c]['x']));
+            ys.push(relinLat(records[key][c]['y']));
+            names.push(`${records[key][c]['nom']} - ${records[key][c]['code_postal']}`);
         }
         data.push({
             x: xs,
@@ -156,12 +174,3 @@ boutonTracer.addEventListener("click", async (event) => {
     selectRes.disabled = false;
     boutonTelecharger.disabled = false;
 })
-
-checkDeps.checked = false // on initialise à rien cocher
-selectRes.value = '1080';
-selectRes.disabled = true;
-boutonTelecharger.disabled = true;
-selectRes.dispatchEvent(new Event("change"));
-checkDeps.dispatchEvent(new Event("change"));
-sliderWidth.dispatchEvent(new Event("change"));
-sliderWidthD.dispatchEvent(new Event("change"));
