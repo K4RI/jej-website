cb6a1dd33152489ffdf77ae898d50b5bd57539c2
K4RI <kariassoumani@hotmail.fr>
2024-04-25 10:50:30 +0200

    autoroute v2 : péage

--- scripts/js/autoroute.js
+++ scripts/js/autoroute.js
@@ -5,6 +5,7 @@ const arrCouleurs = ['♠', '♣', '♥', '♦']
 
 /** @type {Boolean} avec ou sans péage ? */
 var peage = false
+var peageFini = false
 
 /** @type {Number} nombre de parties à simuler */
 var nParties = 10000
@@ -53,10 +54,18 @@ canvasInfos2.innerHTML=' '
 let divButtons = document.createElement('div')
 divButtons.id = 'div-buttons'
 divButtons.setAttribute('style', "display: flex; justify-content: center; align-items: center")
-divButtons.innerHTML = `<input type="button" id="bouton-haut" value="PLUS HAUT" style="font-size: 5vh; font-style: italic; color:black; padding: 10px 20px; margin: 10px"></input><input type="button" id="bouton-bas" value="PLUS BAS" style="font-size: 5vh; font-style: italic; color:black; padding: 10px 20px; margin: 10px"><input type="button" id="bouton-relancer" value="RELANCER UNE PARTIE" style="font-size: 5vh; font-style: italic; color:black; padding: 10px 20px; margin: 10px; display: none;"></input>`
+divButtons.innerHTML = `<input type="button" id="bouton-bas" value="PLUS BAS" style="font-size: 5vh; font-style: italic; padding: 10px 20px; margin: 10px"><input type="button" id="bouton-haut" value="PLUS HAUT" style="font-size: 5vh; font-style: italic; padding: 10px 20px; margin: 10px"></input><input type="button" id="bouton-relancer" value="RELANCER UNE PARTIE" style="font-size: 5vh; font-style: italic; color:black; padding: 10px 20px; margin: 10px; display: none;"></input>`
+let [boutonMoins, boutonPlus, boutonRelancer] = divButtons.childNodes
 
-let [boutonPlus, boutonMoins, boutonRelancer] = divButtons.childNodes
+let divPeage = document.createElement('div')
+divPeage.id = 'div-peage'
+divPeage.setAttribute('style', "display: flex; justify-content: center; align-items: center")
+divPeage.innerHTML = `<form><input type="text" id="entree-peage" placeholder="" maxlength="2" style="font-size: 1.4em; margin: 10px; text-align: center"></input><input type="submit" id="valider-peage" value="Valider" style="margin: 10px"></input><br><i style="font-size: 0.9em; color:red;"></i></form>`
+let validePeage = divPeage.firstChild
+let inputPeage = divPeage.firstChild.firstChild
+let erreurPeage = divPeage.firstChild.childNodes[3]
 
+/** Modifie le nombre de cartes affichées dans le canvasText */
 function changeCanvasSize(n){
     canvasText.innerHTML = ''
     for (let i=0; i<n; i++){
@@ -73,9 +82,10 @@ function canvasInit(){  // affiche toute la partie de gauche pour la partie
     canvas.appendChild(document.createElement("br"))
     canvas.appendChild(canvasInfos2)
     canvas.appendChild(divButtons)
+    canvas.appendChild(divPeage)
     for (let j=0; j<taille; j++){
         canvasText.childNodes[j].classList.remove('selected')
-        updateCard(canvasText.childNodes[j], ligne[j])
+        updateCard(canvasText.childNodes[j], ligne[j], peage && j==(taille-1)/2)
     }
 }
 
@@ -86,11 +96,14 @@ sliderTaille.addEventListener("change", (event) => {
     textTaille.innerHTML = sliderTaille.value;
     taille = parseInt(sliderTaille.value);
     changeCanvasSize(taille)
+    checkPeage.disabled = (taille%2==0)
+    peage = checkPeage.checked && (taille%2==1)
 })
 
 sliderValeurs.addEventListener("change", (event) => {
     textValeurs.innerHTML = sliderValeurs.value;
     N = parseInt(sliderValeurs.value);
+    inputPeage.placeholder = arrValeurs.slice(0, N)
 })
 
 sliderParties.addEventListener("change", (event) => {
@@ -99,10 +112,8 @@ sliderParties.addEventListener("change", (event) => {
     textParties.innerHTML = nParties
 })
 
-sliderParties.addEventListener("change", (event) => {
-    let listeNParties = [100, 200, 500, 1000, 2000, 5000, 10000, 20000, 50000, 100000, 200000, 500000, 1000000] // 13
-    nParties = listeNParties[sliderParties.value];
-    textParties.innerHTML = nParties
+checkPeage.addEventListener("change", (event) => {
+    peage = checkPeage.checked
 })
 
 boutonPlus.addEventListener("click", (event) => {
@@ -110,6 +121,17 @@ boutonPlus.addEventListener("click", (event) => {
 })
 boutonMoins.addEventListener("click", (event) => {
     manche({auto:false, choix:false, d:true})
+})
+
+validePeage.addEventListener("submit", (event) => {
+    event.preventDefault()
+    if(arrValeurs.slice(0, N).includes(inputPeage.value)){
+        let c = inputPeage.value
+        inputPeage.value = ''
+        manche({auto: false, choix: c, v: true, d:true})
+    } else {
+        erreurPeage.innerHTML = 'invalide ! entrer une valeur de ' + arrValeurs.slice(0, N)
+    }
 })
 
 
@@ -138,6 +160,11 @@ function couleur(n){
 function nomCarte(n){
     return `${valeur(n) + couleur(n)}`
 }
+function valeurArray(l){
+    let l2 = l.map(e => valeur(e))
+    if (peage && !peageFini){ l2[(taille-1)/2] = '?' }
+    return l2
+}
 
 /** Effectue une manche d'autoroute.
  *  Modifie les variables paquet, ligne, i, et points.
@@ -150,38 +177,96 @@ function manche({auto = true, choix = false, v = true, d = false}){
     if (paquet.length == 0){
         paquet = shuffle([...Array(4*N).keys()].filter(e => !(e in ligne))) // on re-remplit puis mélange le paquet
     }
-    if (v) console.log(`\nLigne ${i+1}\n${ligne.map(e => valeur(e))} - ${valeur(ligne[i])}`)
-    let carteAComparer = ligne[i]
-    let carteInconnue = paquet.shift()
 
-    if (auto){
-        choix = carteAComparer%N < (N-1)/2 ? true : carteAComparer%N > (N-1)/2 ? false : Math.random() < 0.5
-    } // sinon c'est le clic de bouton qui détermine
-    if (v) console.log(`choix ${choix ? "plus haut" : "plus bas"}... face à ${valeur(carteInconnue)}`)
+    if (peage && !peageFini && i == (taille-1)/2){ // péage
+        if (v) console.log(`\n-----Ligne ${i+1}\n${valeurArray(ligne)} - péage !`)
+        let /** la carte du péage */ carteSecrete = ligne[i]
+        let /** l'indice de la valeur choisie dans arrValeurs, à comparer au péage */ nChoix
+        if (auto){
+            nChoix = Math.floor(N*Math.random())
+            choix = arrValeurs[nChoix]
+        } else {
+            nChoix = arrValeurs.indexOf(choix)
+        }
+        if (v) console.log(`choix ${choix}...`)
 
-    ligne[i] = carteInconnue
-    if (d) canvasText.childNodes[i].classList.remove('selected')
-    if (d) updateCard(canvasText.childNodes[i], carteInconnue)
-    if ((carteInconnue%N > carteAComparer%N && choix) || (carteInconnue%N < carteAComparer%N && !choix)){
-        i++
-        if (v) console.log(`YES on avance -> ${points} pts`)
-        if (d) canvasInfos1.style.color = 'green'
-        if (d) canvasInfos1.innerHTML = `oui ! la carte était ${valeur(carteInconnue)}. on avance d'une case !`
-    } else if (carteInconnue%N == carteAComparer%N){
-        points += 2*(i+1)
-        if (v) console.log(`NOOOON égalité, x2 et on redescend -> ${points} pts`)
-        if (d) canvasInfos1.style.color = 'red'
-        if (d) canvasInfos1.innerHTML = `non... la carte était ${valeur(carteInconnue)}. égalité !? <br>retour au départ avec 2x${(i+1)}=${2*(i+1)} pénalités... - total jusqu'ici : ${points}`
-        i = 0
-    } else {
-        points += (i+1)
-        if (v) console.log(`NON on redescend -> ${points} pts`)
-        if (d) canvasInfos1.style.color = 'orangered'
-        if (d) canvasInfos1.innerHTML = `non... la carte était ${valeur(carteInconnue)}. retour avec +${(i+1)} pénalité${i!=0 ? 's' : ''}... (total : ${points})`
-        i = 0
+        if (d) canvasText.childNodes[i].classList.remove('selected')
+        if (nChoix%N < carteSecrete%N){
+            points += (i+1)
+            if (v) console.log(`${choix} trop bas :( -> ${points} pts`)
+            if (d) {
+                canvasInfos1.style.color = 'red'
+                canvasInfos1.innerHTML = `tu as choisi ${choix}... c'est trop bas.<br>retour au départ avec +${i+1} pénalités... - total jusqu'ici : ${points}`
+                baliseCommentaires.innerHTML += `${choix} trop bas<br>`
+            }
+            i=0
+        } else if (nChoix%N > carteSecrete%N){
+            points += (i+1)
+            if (v) console.log(`${choix} trop haut :( -> ${points} pts`)
+            if (d) {
+                canvasInfos1.style.color = 'red'
+                canvasInfos1.innerHTML = `tu as choisi ${choix}... c'est trop haut.<br>retour au départ avec +${i+1} pénalités... - total jusqu'ici : ${points}`
+                baliseCommentaires.innerHTML += `${choix} trop haut<br>`
+            }
+            i=0
+        } else {
+            points += (i+1)
+            if (v) console.log(`${choix} bonne réponse ! mais tout de même -> ${points} pts`)
+            if (d) {
+                canvasInfos1.style.color = 'green'
+                canvasInfos1.innerHTML = `tu as choisi ${choix}... bravo péage franchi ! mais tout de même<br>retour au départ avec +${i+1} pénalités :) - total jusqu'ici : ${points}`
+                baliseCommentaires.innerHTML += `${choix} réussi ---<br>`
+                updateCard(canvasText.childNodes[i], carteSecrete)
+            }
+            i=0
+            peageFini = true
+        }
+        divButtons.style.display = ''
+        divPeage.style.display = 'none'
+
+    } else { // pas péage
+        if (v) console.log(`\n-----Ligne ${i+1}\n${valeurArray(ligne)} - ${valeur(ligne[i])}`)
+        let carteAComparer = ligne[i]
+        let carteInconnue = paquet.shift()
+
+        if (auto){
+            choix = carteAComparer%N < (N-1)/2 ? true : carteAComparer%N > (N-1)/2 ? false : Math.random() < 0.5
+        } // sinon c'est le clic de bouton qui détermine
+        if (v) console.log(`choix ${choix ? '"plus haut"' : '"plus bas"'}... on retourne ${valeur(carteInconnue)}`)
+
+        ligne[i] = carteInconnue
+        if (d) canvasText.childNodes[i].classList.remove('selected')
+        if (d) updateCard(canvasText.childNodes[i], carteInconnue)
+        if ((carteInconnue%N > carteAComparer%N && choix) || (carteInconnue%N < carteAComparer%N && !choix)){
+            i++
+            if (v) console.log(`YES on avance -> ${points} pts`)
+            if (d) canvasInfos1.style.color = 'green'
+            if (d) canvasInfos1.innerHTML = `oui ! la carte était ${valeur(carteInconnue)}, on avance d'une case !`
+        } else if (carteInconnue%N == carteAComparer%N){
+            points += 2*(i+1)
+            if (v) console.log(`NOOOON égalité, x2 et on redescend -> ${points} pts`)
+            if (d) canvasInfos1.style.color = 'red'
+            if (d) canvasInfos1.innerHTML = `non... la carte était aussi ${valeur(carteInconnue)}, égalité !? <br>retour au départ avec 2x${(i+1)}=${2*(i+1)} pénalités... - total jusqu'ici : ${points}`
+            i = 0
+        } else {
+            points += (i+1)
+            if (v) console.log(`NON on redescend -> ${points} pts`)
+            if (d) canvasInfos1.style.color = 'orangered'
+            if (d) canvasInfos1.innerHTML = `non... la carte était ${valeur(carteInconnue)}. retour avec +${(i+1)} pénalité${i!=0 ? 's' : ''}... (total : ${points})`
+            i = 0
+        }
     }
 
-    if (i<taille){
+    if (peage && !peageFini && i == (taille-1)/2){
+        if (v) console.log("Tut tut c'est le péage !")
+        if (d){
+            canvasText.childNodes[i].classList.add('selected')
+            canvasInfos1.innerHTML += "<br>!! Tut tut, bienvenue au péage !!"
+            canvasInfos2.innerHTML = "Il faut deviner la carte cachée..."
+            divButtons.style.display = 'none'
+            divPeage.style.display = ''
+        }
+    } else if (i<taille){
         if (d) canvasText.childNodes[i].classList.add('selected')
         if (d) canvasInfos2.innerHTML = `case n°${i+1}, on est face à la carte <strong>${valeur(ligne[i])}</strong>`
     } else {
@@ -206,14 +291,25 @@ function initPartie(){
     ligne = paquet.slice(0, taille) // et la première ligne devant
     paquet = paquet.slice(taille, 4*N)
     i = 0, points = 0 // commence à la case 0 avec 0 points
+    erreurPeage.innerHTML = ''
+    inputPeage.value = ''
+    peageFini = false
     partieEnCours = true
 }
 
-/** Met à jour le texte affiché dans la carte sur le canvas */
-function updateCard(c, n){
+/** Met à jour le texte affiché dans la carte sur le canvas
+ * @param {HTMLElement} c l'élément dans lequel écrire
+ * @param {Number} n le numéro de la carte
+ * @param {Boolean} pp est-ce que l'emplacement est sur un péage ? true uniquement dans canvasInit() dans boutonLancer
+*/
+function updateCard(c, n, pp=false){
     c.classList.remove('red', 'black')
-    c.classList.add(n<2*N ? 'black' : 'red')
-    c.innerHTML = nomCarte(n)
+    if (pp){
+        c.innerHTML = '?'
+    } else {
+        c.classList.add(n<2*N ? 'black' : 'red')
+        c.innerHTML = nomCarte(n)
+    }
 }
 
 
@@ -251,10 +347,16 @@ boutonLancer.addEventListener("click", (event) => {
     sliderValeurs.disabled = true
     checkPeage.disabled = true
     boutonReinit.disabled = false
+    divButtons.style.display = ''
+    divPeage.style.display = 'none'
     boutonMoins.style.display = ''
     boutonPlus.style.display = ''
     boutonRelancer.style.display = 'none'
     canvas.innerHTML = '' // on le vide du précédent texte, ou du plotly
+    baliseCommentaires.innerHTML = `<br>valeurs dans le paquet : ${arrValeurs.slice(0,N)}<br><br>`
+    if (peage) {
+        baliseCommentaires.innerHTML += `---tentatives du péage :<br>`
+    }
 
     canvasInit()
     canvasText.childNodes[0].classList.add('selected')
@@ -327,7 +429,7 @@ boutonSimul.addEventListener("click", (event) => {
     sliderValeurs.disabled = false
     checkPeage.disabled = false
     boutonReinit.disabled = true
-    baliseCommentaires.innerHTML = `<i style="font-size: 0.9em; color:red;">chargement...</i>`
+    baliseCommentaires.innerHTML += `<br><i style="font-size: 0.9em; color:red;">chargement...</i>`
     
     setTimeout(() => {
         parties = test()
@@ -338,7 +440,6 @@ boutonSimul.addEventListener("click", (event) => {
         &nbsp; - Médiane : ${parties_sorted[~~(nn/2)]} tours<br>
         &nbsp; - Parties parfaites (zéro pénalité) : ${100*parties.reduce((acc, cur) => acc + (cur==0), 0,)/nParties}%`
         tracer()
-        console.log(partieEnCours)
     },0)
 })
 
@@ -347,6 +448,7 @@ boutonReinit.disabled = true
 sliderTaille.dispatchEvent(new Event("change"));
 sliderValeurs.dispatchEvent(new Event("change"));
 sliderParties.dispatchEvent(new Event("change"));
+checkPeage.dispatchEvent(new Event("change"));
 
 // parties = test()
 // console.log(parties)
