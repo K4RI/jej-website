0b3af5a556583c40a60f13fb2e6926c84a61d28f
K4RI <kariassoumani@hotmail.fr>
2026-06-04 09:46:48 +0200

    headerfooter test
    - js : nouveau script headerfooter.js
    - css : changer ".nav-container" en "nav"
    - test sur les pages contact.html, dessins.html, dessins/avelv.html :
    1) vider le nav en "<nav id="navbar"></nav>"
    2) vider le footer en "<footer id="footer"> <script type="text/javascript" src="scripts/js/headerFooter.js"></script></footer>"
    /!\ ATTENTION aux "../scripts" dans le lfooter /!\

---
+++ scripts/js/headerfooter.js
@@ -0,0 +1,61 @@
+// https://stackoverflow.com/questions/6005327/how-to-create-header-footer
+
+/**************** 1 : NAVBAR */
+const titles = {
+    "a-propos" : "à propos de jej",
+    "dessins": "dessins",
+    "videos" : "vidéos",
+    "images" : "images",
+    "textes" : "textes",
+    "jeux" : "jeux",
+    "postits" : "post-its",
+    "remerciements" : "special thanks"
+}
+
+// sur internet ou en local ?
+const hostname = (window.location.href.substring(0, 5) == "https") ? "jej888.fr" : ":8000"
+
+let subdirs = window.location.href.split(".html")[0].split(hostname).pop().split('/')
+if (subdirs[0] == "") { subdirs.shift() }
+console.log(subdirs) // ["dessins"], ou ["dessins", "avelv"] ...
+
+navbar = document.getElementById("navbar");
+navbar.innerHTML = ""
+
+if (subdirs.length == 1) { // dossier principal
+    Object.keys(titles).forEach(key => {
+        if (subdirs[0] == key) {
+            navbar.innerHTML += `<span class="active">${titles[key]}</span>`;
+        
+        } else {
+            navbar.innerHTML += `<a href="${key}.html"><span>${titles[key]}</span></a>`;
+        }        
+    })
+
+} else if (subdirs.length > 1) { // sous-dossier
+    let prefix = Array(subdirs.length).join("../") // le répète "subdirs.length - 1" fois
+    console.log(prefix)
+    Object.keys(titles).forEach(key => {
+        if (subdirs[0] == key) {
+            navbar.innerHTML += `<a href="${prefix + key}.html" style="background-color: #f9fd83;"><span>${titles[key]}</span></a>`;
+        
+        } else {
+            navbar.innerHTML += `<a href="${prefix + key}.html"><span>${titles[key]}</span></a>`;
+        }        
+    })
+}
+
+
+/**************** 2 : FOOTER */
+footer = document.getElementById("footer");
+footer.innerHTML = `
+    ___________________________
+    <p ><a href="https://www.youtube.com/@jej888" target="_blank">youtube</a> - 
+    <a href="./tweets.html">twitter</a> - 
+    <a href="https://www.instagram.com/nwaar888/" target="_blank">instagram</a> - 
+    mail : jejasfh [at] gmail.com
+    </p>
+    <p>© smoking borzoi studios 2k24-∞</p>
+    <script type="text/javascript" src="scripts/js/headerFooter.js"></script>
+`
+// ajouter des 88x31 comme dans https://manpaint.neocities.org/
