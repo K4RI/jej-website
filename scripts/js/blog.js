84d0f2db6e84400c39dac767b37d3fd26056f05b
K4RI <kariassoumani@hotmail.fr>
2026-06-05 09:35:58 +0200

    blog correctif

--- scripts/js/blog.js
+++ scripts/js/blog.js
@@ -10,18 +10,30 @@ try {
     const text = await response.text();
     text.split("\n\n------------").forEach((entrée) => {
         let pavés = entrée.split("\n\n");
-        hhhh.innerHTML += `<h2>${pavés[0]}</h2>`;
+        if (pavés[0] != "") {
+            hhhh.innerHTML += `<h2>${pavés[0]}</h2>`;
+        }        
 
         pavés.slice(1).forEach((pavé) => {
             let paragraphes = pavé.split("\n");
-            hhhh.innerHTML += `<h3>${paragraphes[0]}</h3>`;
+
+            // le nom du paragraphe
+            let p = paragraphes[0];
+
+            // la balise du paragraphe (id, href)
+            let pp = p.normalize("NFD").toLowerCase().replace(/[\'| ]/g,'-').replace(/[^a-z0-9-]/g,'')
+            
+            var article = document.createElement("article");
+            article.id = pp
+            article.innerHTML += `<h3><a href="#${pp}">${p}</a></h3>`;
     
             paragraphes.slice(1).forEach((paragraphe) => {
                 if (paragraphe != "") {
-                    hhhh.innerHTML += `<p>${paragraphe}</p>`;
+                    article.innerHTML += `<p>${paragraphe}</p>`;
                 }
             })
-            hhhh.innerHTML += `<br>`;
+            article.innerHTML += `<br>`;
+            hhhh.appendChild(article);
         })
     })
 
